37cd66c6db6594ac073cf496336eca46f959c67b
aceofcolours <aceofcolours@gmail.com>
2024-09-26 03:43:43 -0400

    Created variables needed

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,9 +1,15 @@
-﻿using System;
-
-class Variables 
+﻿namespace ConsoleApp2
 {
-    public static void Main(string[] args)
+    internal class Program
     {
-        
+        static void Main(string[] args)
+        {
+            //variables
+            string heroFirstName = "Saavan";
+            string heroLastName = "Tandon";
+            string heroFullName;
+
+            //begin code instructions
+        }
     }
 }
