5bd8a6c4dadcf58aea0cf3b5a66cd7aee6ee59d4
aceofcolours <aceofcolours@gmail.com>
2024-09-26 04:50:13 -0400

    battle descriptions and printing

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -14,11 +14,79 @@
             string minion1Name = "Joe";
             string minion2Name = "Shmoe";
 
+            int heroHealth = 100;
+            int heroStrength = 50;
+            int bossHealth = 100;
+            int bossStrength = 25;
+            int minion1Health = 50;
+            int minion2Health = 50;
+            int minionStrength = 5;
+
             //begin code instructions
             heroFullName = heroFirstName + " " + heroLastName;
 
+            //introductions
             Console.WriteLine("Behold the glorious hero of programming: " + heroFullName + "!");
             Console.WriteLine(heroFullName + "'s archnemesis is " + villainName + " the " + villainTitle + " whose minions are two " + minionTitle + "s, " + minion1Name + " and " + minion2Name + ". But they're no match for a hero as great as " + heroFirstName + "!");
+
+            //fight!
+            Console.WriteLine("The fighters enter the room anyway!");
+            Console.WriteLine("Hero's health: " + heroHealth);
+            Console.WriteLine("Boss's health: " + bossHealth);
+            Console.WriteLine("Minion 1's health: " + minion1Health);
+            Console.WriteLine("Minion 2's health: " + minion2Health);
+
+            //villain attack
+            Console.WriteLine(villainName + " the " + villainTitle + " attacks " + heroFullName);
+            Console.WriteLine(villainName + " the " + villainTitle + " deals " + bossStrength + " damage!");
+            heroHealth = heroHealth - bossStrength;
+            Console.WriteLine(heroFirstName + " now has " + heroHealth + " health");
+
+            //minion attack
+            Console.WriteLine("The minions attack!");
+            Console.WriteLine(minion1Name + " deals " + minionStrength + " damage!");
+            heroHealth = heroHealth - minionStrength;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health");
+            Console.WriteLine(minion2Name + " deals " + minionStrength + " damage!");
+            heroHealth = heroHealth - minionStrength;
+            Console.WriteLine(heroFirstName + " now has " + heroHealth + " health");
+
+            //hero attack
+            Console.WriteLine(heroFullName + " attacks all enemies for " + heroStrength + " damange!");
+            bossHealth -= heroStrength;
+            minion1Health -= heroStrength;
+            minion2Health -= heroStrength;
+
+            //display health
+            Console.WriteLine(villainName + " the " + villainTitle + " now has " + bossHealth + " health!");
+            Console.WriteLine(minionTitle + " " + minion1Name + " now has " + minion1Health + " health!");
+            Console.WriteLine(minionTitle + " " + minion2Name + " now has " + minion2Health + " health!");
+            Console.WriteLine(minion1Name + " and " + minion2Name + " have been defeated!");
+
+            //end of round buff
+            Console.WriteLine(heroFullName + " has " + heroHealth + " health");
+            Console.WriteLine(heroFullName + " eats an apple to restore health");
+            heroHealth++;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health");
+
+            //final round of fight
+            Console.WriteLine("Hero health: " + heroHealth);
+            Console.WriteLine("Boss health: " + bossHealth);
+
+            //villain attack
+            Console.WriteLine(villainName + " the " + villainTitle + " attacks " + heroFullName);
+            Console.WriteLine(villainName + " the " + villainTitle + " deals " + bossStrength + " damage!");
+            heroHealth = heroHealth - bossStrength;
+            Console.WriteLine(heroFirstName + " now has " + heroHealth + " health");
+
+            //hero attack
+            Console.WriteLine(heroFullName + " attacks the " + villainTitle + " for " + heroStrength + " damange!");
+            bossHealth -= heroStrength;
+            Console.WriteLine("The boss now has " + bossHealth + " health!");
+            Console.WriteLine(heroFullName + " is victorious!");
+            Console.WriteLine("Wealth, glory, and straight A's to the winner!");
+
+            //end
             Console.WriteLine("Press any key to close this window...");
             Console.ReadKey();
         }
--- Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
+++ Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7875ffbcb656b8d493c1820fcdee7727e8aef603")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0d9d62048dccd0201fb1a6ce7d374d411107548")]
 [assembly: System.Reflection.AssemblyProductAttribute("Variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
